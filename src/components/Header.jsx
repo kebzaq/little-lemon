1b4d21f1dcb6ae40bcf1196b1efb3040b472fc02
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-04-28 21:54:44 -0400

    navbar component updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header id="header">
-      <div className="hero">
-        <h1>Little Lemon</h1>
-        <h4>Chicago</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit rerum
-          tenetur temporibus sequi iusto obcaecati eum ipsa illo at facere
-          voluptatem vel impedit, omnis id doloremque natus eligendi sunt
-          quaerat.
-        </p>
-        <button>Reserve a Table</button>
-      </div>
-      <div className="hero">
-        <img src="" alt="little lemon restaurant image" />
+    <header id="callforaction container">
+      <div className="row justify-content-lg-center">
+        <div className="hero col col-xl-4">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+            rerum tenetur temporibus sequi iusto obcaecati eum ipsa illo at
+            facere voluptatem vel impedit, omnis id doloremque natus eligendi
+            sunt quaerat.
+          </p>
+          <button>Reserve a Table</button>
+        </div>
+        <div className="hero col col-xl-4">
+          <img src="" alt="little lemon restaurant image" />
+        </div>
       </div>
     </header>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,45 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav id="navbar">
-      <div className="logo">
-        <img
-          src="https://little-lemon-restaurant-project.netlify.app/static/media/Logo.6c132dfefa69396bba43f72aa595e62f.svg"
-          alt="Little Lemon logo"
-        />
-      </div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Menu</li>
-        <li>Reservations</li>
-        <li>Order Online</li>
-        <li>Login</li>
+    <nav className="container">
+      <ul className="nav col-xl-9 justify-content-md-center">
+        <li className="nav-item">
+          <img
+            id="logo"
+            src="https://little-lemon-restaurant-project.netlify.app/static/media/Logo.6c132dfefa69396bba43f72aa595e62f.svg"
+            alt="Little Lemon logo"
+          />
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link active">
+            Home
+          </a>
+        </li>
+        <li className="nav">
+          <a href="#" className="nav-link">
+            About
+          </a>
+        </li>
+        <li className="nav">
+          <a href="#" className="nav-link">
+            Menu
+          </a>
+        </li>
+        <li className="nav">
+          <a href="#" className="nav-link">
+            Reservations
+          </a>
+        </li>
+        <li className="nav">
+          <a href="#" className="nav-link">
+            Order Online
+          </a>
+        </li>
+        <li className="nav">
+          <a href="#" className="nav-link">
+            Login
+          </a>
+        </li>
       </ul>
     </nav>
   );
